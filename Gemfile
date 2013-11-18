d33260b97875b9605aed668d80eb7b6085a43ddb
Ross Hays <accounts@rosshays.net>
2013-11-17 21:08:42 -0500

    initial project setup

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'rails', '3.2.13'
+
+# Development gems used to aid development but not deployed
+group :development do
+	gem 'sqlite3'
+end 
+
+# Deployment gems
+group :production do
+	gem 'pg'
+end
+
+# Gems used only for assets and not required in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = '3ce61f88c8700eff494fc0dab22968a47c4aa8ace5762cd90051fe225663334e080ba7e6fb6276f85b199de185a45849eb48723c263498f69b746ebd99ea046e'
