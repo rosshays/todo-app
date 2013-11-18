8f20bdfa621748788d9bf8bf9e2f447aaa661eb4
Ross Hays <dev@rosshays.net>
2013-11-18 13:41:52 -0500

    added migration to change password to digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131118183347) do
+ActiveRecord::Schema.define(:version => 20131118184110) do
 
   create_table "task_list_users", :force => true do |t|
     t.integer  "user_id"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(:version => 20131118183347) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "fullname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
