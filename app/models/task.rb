70282b2b7f313424b399bf8337feb48ada2b3f35
Ross Hays <dev@rosshays.net>
2013-11-21 17:42:17 -0500

    what's done on tasks so far

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
-  attr_accessible :duedate, :task, :task_list_id
+  attr_accessible :duedate, :task, :task_list_id, :has_due_date
   belongs_to :task_list
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131121210059) do
+ActiveRecord::Schema.define(:version => 20131121215655) do
 
   create_table "task_list_users", :force => true do |t|
     t.integer  "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20131121210059) do
     t.integer  "task_list_id"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.boolean  "has_due_date"
   end
 
   create_table "users", :force => true do |t|
