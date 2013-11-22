43ecf7d2c139650b4470d557222729ba1acbc9f6
Ross Hays <dev@rosshays.net>
2013-11-21 21:27:15 -0500

    fixed bug with changing value of priority prior to save

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   attr_accessible :duedate, :task, :task_list_id, :has_due_date, :priority
   belongs_to :task_list
-  before_save { self.priority = self.priority - 1 }
+  before_save { self.priority = (self.priority.to_i - 2).to_s  }
 end
