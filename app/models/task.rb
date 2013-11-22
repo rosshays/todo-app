ada863db8c28bc516116d25f4f1d33793fe2bd2c
Ross Hays <dev@rosshays.net>
2013-11-21 21:14:47 -0500

    added priority and date

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
-  attr_accessible :duedate, :task, :task_list_id, :has_due_date
+  attr_accessible :duedate, :task, :task_list_id, :has_due_date, :priority
   belongs_to :task_list
 end
