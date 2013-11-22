e10626feae31494f4783d13582b3dd1e1db0e578
Ross Hays <dev@rosshays.net>
2013-11-21 21:19:00 -0500

    running migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131122020353) do
+ActiveRecord::Schema.define(:version => 20131122021554) do
 
   create_table "task_list_users", :force => true do |t|
     t.integer  "user_id"
@@ -30,10 +30,11 @@ ActiveRecord::Schema.define(:version => 20131122020353) do
     t.string   "task"
     t.date     "duedate"
     t.integer  "task_list_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
     t.boolean  "has_due_date"
     t.string   "priority"
+    t.boolean  "finished",     :default => false
   end
 
   create_table "users", :force => true do |t|
